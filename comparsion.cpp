da3b38316bea61a7a97cd0b3fd20359278252d78
Abhishek <114054962+abhi5hek-01@users.noreply.github.com>
2023-01-03 12:54:53 +0530

    Create comparsion.cpp

---
+++ comparsion.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin>>T;
+    for(int i=0;i<=T;i++){
+        int a,b;
+        cin>>a>>b;
+        if(a>b){
+            cout<<">"<<endl;
+        }
+        else if(a<b&&b>a)
+        {
+
+            cout<<"<"<<endl;
+           
+        }
+        else if(a=b)
+        {
+            cout<<"="<<endl;
+        }
+       
+    }
+     return 0;
+     
+}
