7f47baf4261a53c7416cac71862bc919e8ab8795
Abhishek <114054962+abhi5hek-01@users.noreply.github.com>
2023-01-24 16:27:42 +0530

    Create inorder.cpp

---
+++ TREE/leetcode tree traversal/inorder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+vector<int>v;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root!=NULL){
+
+    
+     inorderTraversal(root->left);
+     v.push_back(root->val);
+     inorderTraversal(root->right);
+        
+ }
+
+ return v;
+    }  
+};
