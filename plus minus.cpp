9460b90473580609f86bd52ecc7e3850fa0cf491
Abhishek <114054962+abhi5hek-01@users.noreply.github.com>
2023-02-06 00:00:22 +0530

    Update plus minus.cpp

--- plus minus.cpp
+++ plus minus.cpp
@@ -1,1 +1,27 @@
-.
+void plusMinus(vector<int> arr) {
+  float pluscnt=0;
+   float minuscnt=0;
+   float  zerocnt=0;
+   int n=arr.size();
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<0)
+        {
+         minuscnt++;
+        } 
+        else if (arr[i]==0) {
+           zerocnt ++;
+        }
+        else if(arr[i]>0)
+        {
+            pluscnt++;
+        }   
+    }
+    double result=pluscnt/n;
+     cout<<result<<endl;
+     double output=minuscnt/n;
+       cout<<output<<endl;
+      double last=zerocnt/n;
+      cout<<last<<endl;   
+         
+}
